1366cb6e9a57d466ad0f4c15126694d4ec05b014
Chris Ballard <chris.ballard@gmail.com>
2018-06-03 23:25:43 -0700

    lambda function edits

--- lambda.py
+++ lambda.py
@@ -7,5 +7,17 @@ def lambda_handler(event, context):
     response = client.publish(
         topic='bedcommands',
         qos=0,
-        payload=str(event['bed_pos'])
+        payload=str(event['request']['intent']['slots']['bed_pos']['value'])
     )
+    
+    response = {
+        'version': '1.0',
+        'response': {
+            'outputSpeech': {
+                'type': 'PlainText',
+                'text': 'done',
+            }
+        }
+    }
+    return response
+    
